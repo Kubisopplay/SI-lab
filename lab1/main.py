b2a010912106f5135a2dd11d9a435ae20a7b67f8
Jakub Oleszczuk <38842052+Kubisopplay@users.noreply.github.com>
2023-04-05 16:43:44 +0200

    raeeee

--- lab1/main.py
+++ lab1/main.py
@@ -6,7 +6,7 @@ import pandas as pd
 import multiprocessing
 import math
 from helper import Node, get_distance,set_przystanki
-from zad1 import zad1
+from zad1 import zad1, set_globals
 import cProfile
 import timeit
 
@@ -39,9 +39,9 @@ print(min_latitude, min_longitude, max_latitude, max_longitude)
 cProfile.run('zad1("PL. GRUNWALDZKI", "DWORZEC GŁÓWNY", przystanki, data, "10:00:00")', "wynik.txt")
 #print(zad1( "DWORZEC GŁÓWNY","PL. GRUNWALDZKI", przystanki, data, "20:00:00"))
 cProfile.run('zad1("Komuny Paryskiej", "Wilkszyn - Polna", przystanki, data, "01:39:39")', "wynik2.txt")
-random.seed(2137)
+random.seed(123)
 test_pairs = []
-for i in range(10):
+for i in range(3):
     start = list(przystanki)[random.randint(0, len(przystanki)-1)]
     end = list(przystanki)[random.randint(0, len(przystanki)-1)]
     while end == start:
@@ -56,8 +56,41 @@ default = 0
 for start, finish, hour in test_pairs:
     print("start ", start, "finish ", finish, "hour ", hour)
     default = default + timeit.timeit(lambda: zad1(start, finish, przystanki, data, hour), number=1)
+djikstra = 0
 
 
+#focused on time
+timefocus = 0
+set_globals(1,0,10,3,0,0)
+for start, finish, hour in test_pairs:
+    timefocus += timeit.timeit(lambda: zad1(start, finish, przystanki, data, hour), number=1)
+
+
+
+#focused on distance
+distancefocus = 0
+set_globals(1,0,10,0,5,0)
+for start, finish, hour in test_pairs:
+    distancefocus += timeit.timeit(lambda: zad1(start, finish, przystanki, data, hour), number=1)
+
+#transferfocus
+transferfocus = 0
+set_globals(1,0,10,0,0,1000)
+for start, finish, hour in test_pairs:
+    transferfocus += timeit.timeit(lambda: zad1(start, finish, przystanki, data, hour), number=1)
+
+print(default, " Default values")
+print(timefocus, "Astar time focused")
+print(distancefocus, "Astar distance focused")
+print(transferfocus, "Astar transfer focused")
+
+set_globals(1,0,10,0,0,100)
+for start, finish, hour in test_pairs:
+    djikstra += timeit.timeit(lambda: zad1(start, finish, przystanki, data, hour), number=1)
+
+print(default, " Default values")
+print(timefocus, "Astar time focused")
+print(djikstra,"Djikstra")
 
 
 
